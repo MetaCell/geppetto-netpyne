5a321a67ee6c3671ffd733287c86936fc9225ad6
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2016-11-07 13:25:56 -0800

    Fixing CSS, adding other components

--- ComponentsInitialization.js
+++ ComponentsInitialization.js
@@ -10,15 +10,31 @@ define(function (require) {
 	    
 		//Logo initialization 
 		GEPPETTO.ComponentFactory.addComponent('LOGO', {logo: 'gpt-gpt_logo'}, document.getElementById("geppettologo"));
-		
+
+		//Control panel initialization
+		GEPPETTO.ComponentFactory.addComponent('CONTROLPANEL', {}, document.getElementById("controlpanel"));
+
+		//Spotlight initialization
+		GEPPETTO.ComponentFactory.addComponent('SPOTLIGHT', {}, document.getElementById("spotlight"));
+
+		//Create python console
+		var pythonNotebookPath = "http://localhost:8888/notebooks/libs/neuron-ui-demo.ipynb";
+        GEPPETTO.ComponentFactory.addComponent('PYTHONCONSOLE', {pythonNotebookPath: pythonNotebookPath}, document.getElementById("pythonConsole"));
+
+		//Experiments table initialization
+		GEPPETTO.ComponentFactory.addComponent('EXPERIMENTSTABLE', {}, document.getElementById("experiments"));
+
+		//Simulation controls initialization
+		GEPPETTO.ComponentFactory.addComponent('SIMULATIONCONTROLS', {}, document.getElementById("sim-toolbar"));
+
+		//Foreground initialization
+		GEPPETTO.ComponentFactory.addComponent('FOREGROUND', {dropDown : false}, document.getElementById("foreground-toolbar"));
+
 		//Customise layout
 		$("#github").hide();
 		$("#sim").css("background-color","#3e2723");
 		
-		//Create python console
-		var pythonNotebookPath = "http://localhost:8888/notebooks/libs/neuron-ui-demo.ipynb";
-        GEPPETTO.ComponentFactory.addComponent('PYTHONCONSOLE', {pythonNotebookPath: pythonNotebookPath}, document.getElementById("pythonConsole"));
-		
+
         //Remove idle time out warning
         GEPPETTO.G.setIdleTimeOut(-1);
         
