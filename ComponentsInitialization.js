3e387e454561ec1f3e5921a1825c7f6fd1083da0
Adrian Quintana <adrianquintana@gmail.com>
2017-02-01 10:34:36 +0000

    add control menu button, neuron specific css, starting to work with brightness fucntion

--- ComponentsInitialization.js
+++ ComponentsInitialization.js
@@ -8,6 +8,13 @@ define(function (require) {
         link.href = "geppetto/extensions/geppetto-neuron/css/material.css";
         document.getElementsByTagName("head")[0].appendChild(link);
 
+
+        var link = document.createElement("link");
+        link.type = "text/css";
+        link.rel = "stylesheet";
+        link.href = "geppetto/extensions/geppetto-neuron/css/neuron.css";
+        document.getElementsByTagName("head")[0].appendChild(link);
+
         //Logo initialization
         GEPPETTO.ComponentFactory.addComponent('LOGO', { logo: 'gpt-gpt_logo' }, document.getElementById("geppettologo"));
 
@@ -75,6 +82,83 @@ define(function (require) {
 
         //Experiments table initialization
         GEPPETTO.ComponentFactory.addComponent('EXPERIMENTSTABLE', {}, document.getElementById("experiments"));
+
+        window.getRecordedMembranePotentials = function() {
+            return Project.getActiveExperiment().getWatchedVariables(true, false);
+            // var instances = Project.getActiveExperiment().getWatchedVariables(true, false);
+            // var v = [];
+            // for (var i = 0; i < instances.length; i++) {
+            //     if (instances[i].getInstancePath().endsWith(".v")) {
+            //         v.push(instances[i]);
+            //     }
+            // }
+            // return v;
+        };
+
+        // window.addBrightnessFunction = function(){
+        //     for(var membranePotentialsIndex in window.getRecordedMembranePotentials()){
+        //         //modulation = statevariable
+        //         //instance =geometry
+        //             G.addBrightnessListener(instance, modulation, normalizationFunction)
+
+        //             }
+        // }
+
+        var configuration = {
+            id: "controlsMenuButton",
+            openByDefault: false,
+            closeOnClick: false,
+            label: ' Results',
+            iconOn: 'fa fa-caret-square-o-up',
+            iconOff: 'fa fa-caret-square-o-down',
+            menuPosition: {
+                top: 40,
+                right: 550
+            },
+            menuSize: {
+                height: "auto",
+                width: 300
+            },
+            menuItems: [{
+                label: "Plot all recorded variables",
+                action: "window.plotAllRecordedVariables();",
+                value: "plot_recorded_variables"
+            }, {
+                label: "Play step by step",
+                action: "Project.getActiveExperiment().play({step:1});",
+                value: "play_speed_1"
+            }, {
+                label: "Play step by step (10x)",
+                action: "Project.getActiveExperiment().play({step:10});",
+                value: "play_speed_10"
+            }, {
+                label: "Play step by step (100x)",
+                action: "Project.getActiveExperiment().play({step:100});",
+                value: "play_speed_100"
+            }, {
+                label: "Apply voltage colouring to morphologies",
+                condition: "GEPPETTO.G.isBrightnessFunctionSet()",
+                value: "apply_voltage",
+                false: {
+                    action: "G.addBrightnessFunctionBulkSimplified(window.getRecordedMembranePotentials(), function(x){return (x+0.07)/0.1;});"
+                    
+                },
+                true: {
+                    action: "G.removeBrightnessFunctionBulkSimplified(window.getRecordedMembranePotentials(),false);"
+                }
+            }, {
+                label: "Show simulation time",
+                action: "G.addWidget(5).setName('Simulation time').setVariable(time);",
+                value: "simulation_time"
+            }]
+        };
+
+
+
+        //Home button initialization
+         GEPPETTO.ComponentFactory.addComponent('CONTROLSMENUBUTTON', {
+                configuration: configuration
+        }, document.getElementById("ControlsMenuButton"), function(comp){window.controlsMenuButton = comp;});
 
         //Simulation controls initialization
         GEPPETTO.ComponentFactory.addComponent('SIMULATIONCONTROLS', {}, document.getElementById("sim-toolbar"));
