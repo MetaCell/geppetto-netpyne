541a31de29c3d13e35145b83b7c0e60dda4b92cb
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2017-10-03 14:29:10 +0100

    Help dialog

--- components/general/NetPyNEField.js
+++ components/general/NetPyNEField.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 var Utils = require('../../Utils');
 
 export default class NetPyNEField extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            openHelp:false
+        };
 
     }
 
-    showHelp(help) {
-        alert(help)
-    }
+    handleOpenHelp = (help) => {
+        this.setState({ openHelp: true, helpText: help });
+    };
+
+    handleCloseHelp = () => {
+        this.setState({ openHelp: false });
+    };
+
+
 
     render() {
         var help = Utils.getMetadataField(this.props.id, "help");
         if (help != undefined) {
             // var helpComponent = <div>{help}</div>
-            var helpComponent = <div className="helpIcon" onClick={() => this.showHelp(help)}><i className="fa fa-question" aria-hidden="true"></i></div>
+            var helpComponent = <div className="helpIcon" onClick={() => this.handleOpenHelp(help)}><i className="fa fa-question" aria-hidden="true"></i>
+                <Dialog
+                    title="NetPyNE help"
+                    actions={<FlatButton
+                        label="Got it"
+                        primary={true}
+                        keyboardFocused={true}
+                        onClick={this.handleCloseHelp}
+                    />}
+                    modal={true}
+                    open={this.state.openHelp}
+                    onRequestClose={this.handleCloseHelp}
+                >
+                    {this.state.helpText}
+                </Dialog>
+            </div>
         }
 
         return (
