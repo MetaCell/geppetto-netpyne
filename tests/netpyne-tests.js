92eba7985ed50d896e93fc04b8bca3c18584b6b4
jesus <jesus@Y40-70>
2018-02-20 11:27:28 -0600

    test fixing

--- tests/netpyne-tests.js
+++ tests/netpyne-tests.js
@@ -3,7 +3,7 @@ if(urlBase==null || urlBase==undefined){
     urlBase = "http://localhost:8888/";
 }
 
-casper.test.begin('Geppetto default projects tests', function suite(test) {
+casper.test.begin('NetPyNE projects tests', function suite(test) {
 	casper.options.viewportSize = {
 			width: 1340,
 			height: 768
@@ -21,7 +21,7 @@ casper.test.begin('Geppetto default projects tests', function suite(test) {
 		}
 	});
 
-	casper.start(urlBase, function () {
+	casper.start(urlBase+"geppetto", function () {
 		this.echo("Load : " + urlBase);
 		 this.waitWhileVisible('div[id="loading-spinner"]', function () {
 		        this.echo("I've waited for netpyne to load.");
