31484a51837d6d9c8a0eccd38ae49dce7772096e
Adrian Quintana <adrianquintana@gmail.com>
2018-02-22 19:43:15 +0000

    add validation for list

--- components/general/List.js
+++ components/general/List.js
@@ -18,25 +18,67 @@ export default class ListComponent extends Component {
 
         this.addChild = this.addChild.bind(this);
         this.handleNewItemChange = this.handleNewItemChange.bind(this);
-        this.handleChildChange = this.handleChildChange.bind(this);
+        // this.handleChildChange = this.handleChildChange.bind(this);
+    }
+
+    isValid(value) {
+        switch (this.props.realType) {
+            case 'list(float)':
+                var valid = (value.match(/^-?\d*(\.\d+)?$/));
+                break;
+            case 'list(list(float))':
+                var valid = true;
+                value.split(",").forEach(element => {
+                    if (!element.match(/^-?\d*(\.\d+)?$/)) {
+                        valid = false;
+                    }
+                });
+                break;
+            default:
+                var valid = true;
+                break;
+        }
+        return valid;
+    }
+
+    getErrorMessage() {
+        switch (this.props.realType) {
+            case 'list(float)':
+                var message = 'Only numbers are allowed';
+                break;
+            case 'list(list(float))':
+                var message = 'Only array of numbers are allowed (commas, dots and numbers)';
+                break;
+            default:
+                var message = 'No valid value';
+                break;
+        }
+        return message;
     }
 
     handleNewItemChange(event) {
-        this.setState({ newItemValue: event.target.value })
+        this.setState({ newItemValue: event.target.value, newItemErrorText: '' })
     }
 
-    handleChildChange(event, value) {
-        var children = this.state.children
-        children[event.target.id] = value;
-        // Call to conversion function
-        this.convertToPython(children);
-    }
+    // handleChildChange(event, value) {
+    //     if (value != '' && this.isValid(value)) {
+    //         var children = this.state.children
+    //         children[event.target.id] = value;
+    //         // Call to conversion function
+    //         this.convertToPython(children);
+    //     }
+    // }
 
     addChild() {
-        var children = this.state.children;
-        children.push(this.state.newItemValue);
-        // Call to conversion function
-        this.convertToPython(children);
+        if (this.state.newItemValue != '' && this.isValid(this.state.newItemValue)) {
+            var children = this.state.children;
+            children.push("[" + this.state.newItemValue + "]");
+            // Call to conversion function
+            this.convertToPython(children);
+        }
+        else {
+            this.setState({ newItemErrorText: this.getErrorMessage() })
+        }
     }
 
     removeChild(childIndex) {
@@ -90,8 +132,14 @@ export default class ListComponent extends Component {
     render() {
 
         const childrenWithExtraProp = this.state.children.map((child, i) => {
-            return <div key={i.toString()} style={{ marginRight: 10, float: 'left' }}>
-                <TextField value={this.state.children[i]} id={i.toString()} onChange={this.handleChildChange} style={{ width: 60 }} />
+            return <div key={i.toString()} style={{ marginRight: 30, float: 'left' }}>
+                <TextField
+                    value={this.state.children[i]}
+                    id={i.toString()}
+                    // onChange={this.handleChildChange}
+                    style={{ width: 60 }}
+                    disabled
+                />
                 <IconButton
                     iconStyle={{ width: 7, height: 7 }}
                     className={"listButtonSmall"}
@@ -109,14 +157,17 @@ export default class ListComponent extends Component {
                     onChange={this.handleNewItemChange}
                     value={this.state.newItemValue}
                     style={{ width: '100%' }}
+                    errorText={this.state.newItemErrorText}
                 />
-                <IconButton
-                    iconStyle={{ width: 10, height: 10 }}
-                    className={"listButtonLarge"}
-                    onClick={this.addChild}
-                >
-                    <FontIcon className={"fa fa-plus-circle listIcon"} />
-                </IconButton>
+                {!this.state.newItemErrorText &&
+                    <IconButton
+                        iconStyle={{ width: 10, height: 10 }}
+                        className={"listButtonLarge"}
+                        onClick={this.addChild}
+                    >
+                        <FontIcon className={"fa fa-plus-circle listIcon"} />
+                    </IconButton>
+                }
 
                 {childrenWithExtraProp.length > 0 && <div style={{ border: '1px solid rgba(0, 0, 0, 0.3)', padding: '0px 5px', float: 'left' }}>{childrenWithExtraProp}</div>}
             </div>
