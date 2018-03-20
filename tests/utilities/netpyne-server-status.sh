67fab45c09894c93b14413d5e655a0f83850ada6
jesus <yeshuarmartinez@gmail.com>
2018-03-20 11:26:08 -0600

    restore change

--- tests/utilities/netpyne-server-status.sh
+++ tests/utilities/netpyne-server-status.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 iterations=1
 Test () {
-	    request_cmd="$(curl -i -o - --silent -X GET --header 'Accept: application/json' --header 'Authorization: _your_auth_code=='http://localhost:8888/geppetto')"
+	    request_cmd="$(curl -i -o - --silent -X GET --header 'Accept: application/json' --header 'Authorization: _your_auth_code==' 'http://localhost:8888/geppetto')"
 		http_status=$(echo "$request_cmd" | grep HTTP |  awk '{print $2}')
 		echo $http_status
 		if [ "$http_status" == "200" ]; then
