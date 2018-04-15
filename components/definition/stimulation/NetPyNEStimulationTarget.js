ffc85402bb998b3dfcd75aa98d46b1d20e2d1add
rodriguez-facundo <f.bruseghini@gmail.com>
2018-04-15 12:45:44 -0300

    add stim sorce fields

--- components/definition/stimulation/NetPyNEStimulationTarget.js
+++ components/definition/stimulation/NetPyNEStimulationTarget.js
@@ -10,20 +10,30 @@ import RaisedButton from 'material-ui/RaisedButton';
 import clone from 'lodash.clone';
 import Utils from '../../../Utils';
 import NetPyNEField from '../../general/NetPyNEField';
+import ListComponent from '../../general/List';
 
 var PythonControlledCapability = require('../../../../../js/communication/geppettoJupyter/PythonControlledCapability');
 var PythonControlledTextField = PythonControlledCapability.createPythonControlledControl(TextField);
+var PythonControlledSelectField = PythonControlledCapability.createPythonControlledControl(SelectField);
+var PythonControlledListComponent = PythonControlledCapability.createPythonControlledControl(ListComponent);
+var PythonMethodControlledSelectField = PythonControlledCapability.createPythonControlledControlWithPythonDataFetch(SelectField);
 
 export default class NetPyNEStimulationTarget extends React.Component {
 
   constructor(props) {
     super(props);
-    var _this = this;
     this.state = {
-      currentName: props.name
+      currentName: props.name,
+      currentSource: null
     };
-  }
-
+  };
+  
+  componentWillReceiveProps(nextProps) {
+    if (this.state.currentName!=nextProps.name) {
+      this.setState({ currentName: nextProps.name, currentSource: null});
+    };
+  };
+  
   handleRenameChange = (event) => {
     var that = this;
     var storedValue = this.props.name;
@@ -34,8 +44,7 @@ export default class NetPyNEStimulationTarget extends React.Component {
       Utils.renameKey('netParams.stimTargetParams', storedValue, newValue, (response, newValue) => { that.renaming=false;});
       that.renaming=true;
     });
-
-  }
+  };
 
   triggerUpdate(updateMethod) {
     //common strategy when triggering processing of a value change, delay it, every time there is a change we reset
@@ -43,42 +52,43 @@ export default class NetPyNEStimulationTarget extends React.Component {
       clearTimeout(this.updateTimer);
     }
     this.updateTimer = setTimeout(updateMethod, 1000);
-  }
+  };
 
   componentWillReceiveProps(nextProps) {
     this.setState({ currentName: nextProps.name});
-  }
+  };
 
-  render() {
-    var that = this;
-    var content = (<div>
-
-      <TextField
-        onChange={this.handleRenameChange}
-        value = {this.state.currentName}
-        disabled={this.renaming}
-        floatingLabelText="The name of the Stimulation target"
-        className={"netpyneField"}
+  postProcessMenuItems(pythonData, selected) {
+    return pythonData.map((name) => (
+      <MenuItem
+        key={name}
+        insetChildren={true}
+        checked={selected && selected.indexOf(name) > -1}
+        value={name}
+        primaryText={name}
       />
-
-      <br/>
-{/* 
-      <NetPyNEField id="netParams.cellParams.conds.cellModel" >
-        <PythonControlledTextField model={"netParams.cellParams['" + this.props.name + "']['conds']['cellModel']"} />
-      </NetPyNEField>
-
-      <NetPyNEField id="netParams.cellParams.conds.cellType" >
-        <PythonControlledTextField model={"netParams.cellParams['" + this.props.name + "']['conds']['cellType']"} />
-      </NetPyNEField>
-      <br /><br />
-
-      <RaisedButton
-        label="Sections"
-        labelPosition="before"
-        primary={true}
-        onClick={() => that.props.selectPage("sections")}
-      /> */}
-    </div>);
+    ));
+  };
+  
+  render() {
+    var content = (
+      <div>
+        <TextField
+          onChange={this.handleRenameChange}
+          value = {this.state.currentName}
+          disabled={this.renaming}
+          className={"netpyneField"}
+          id={"targetName"}
+        />
+        <br/>
+        <NetPyNEField id={"netParams.stimTargetParams.source"} >
+          <PythonMethodControlledSelectField
+            model={"netParams.stimTargetParams['" + this.props.name + "']['source']"}
+            method={"netParams.stimSourceParams.keys()"}
+            multiple={false}
+          />
+        </NetPyNEField>
+      </div>);
 
     return (
       <div>
@@ -86,4 +96,4 @@ export default class NetPyNEStimulationTarget extends React.Component {
       </div>
     );
   }
-}
+};
