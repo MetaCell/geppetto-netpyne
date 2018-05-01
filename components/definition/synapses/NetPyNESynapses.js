2c9d04018d00e4ca7c4b6e431a45e4ddb910e159
Adrian Quintana <adrianquintana@gmail.com>
2018-05-01 12:27:37 +0100

    remove unneeded variable to avoid undefined

--- components/definition/synapses/NetPyNESynapses.js
+++ components/definition/synapses/NetPyNESynapses.js
@@ -67,7 +67,7 @@ export default class NetPyNESynapses extends React.Component {
     var itemRenamed = this.hasSelectedSynapseBeenRenamed(this.state, nextState) != false;
     var newItemCreated = false;
     var selectionChanged = this.state.selectedSynapse != nextState.selectedSynapse;
-    if (this.state.value!=undefined) {
+    if (this.state.value != undefined) {
       newItemCreated = Object.keys(this.state.value).length != Object.keys(nextState.value).length;
     };
     return newItemCreated || itemRenamed || selectionChanged;
@@ -80,31 +80,9 @@ export default class NetPyNESynapses extends React.Component {
       Synapses.push(<NetPyNESynapseThumbnail name={c} key={c} selected={c == this.state.selectedSynapse} handleClick={this.selectSynapse} />);
     };
     var selectedSynapse = undefined;
-    if (this.state.selectedSynapse && Object.keys(model).indexOf(this.state.selectedSynapse)>-1) {
+    if (this.state.selectedSynapse && Object.keys(model).indexOf(this.state.selectedSynapse) > -1) {
       selectedSynapse = <NetPyNESynapse name={this.state.selectedSynapse} />;
     };
-
-    content = (
-      <CardText className={"tabContainer"} expandable={true}>
-        <div className={"details"}>
-          {selectedSynapse}
-        </div>
-        <div className={"thumbnails"}>
-          <div className="breadcrumb">
-            <IconMenu style={{ float: 'left', marginTop: "12px", marginLeft: "18px" }}
-              iconButtonElement={
-                <NetPyNENewSynapse handleClick={this.handleNewSynapse} />
-              }
-              anchorOrigin={{ horizontal: 'left', vertical: 'top' }}
-              targetOrigin={{ horizontal: 'left', vertical: 'top' }}
-            >
-            </IconMenu>
-          </div>
-          <div style={{ clear: "both" }}></div>
-          {Synapses}
-        </div>
-      </CardText>
-    );
 
     return (
       <Card style={{ clear: 'both' }}>
@@ -115,7 +93,25 @@ export default class NetPyNESynapses extends React.Component {
           showExpandableButton={true}
           id={"Synapses"}
         />
-        {content}
+        <CardText className={"tabContainer"} expandable={true}>
+          <div className={"details"}>
+            {selectedSynapse}
+          </div>
+          <div className={"thumbnails"}>
+            <div className="breadcrumb">
+              <IconMenu style={{ float: 'left', marginTop: "12px", marginLeft: "18px" }}
+                iconButtonElement={
+                  <NetPyNENewSynapse handleClick={this.handleNewSynapse} />
+                }
+                anchorOrigin={{ horizontal: 'left', vertical: 'top' }}
+                targetOrigin={{ horizontal: 'left', vertical: 'top' }}
+              >
+              </IconMenu>
+            </div>
+            <div style={{ clear: "both" }}></div>
+            {Synapses}
+          </div>
+        </CardText>
       </Card>
     );
   }
