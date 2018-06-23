0172f9ac46d39bdbbc946493da067ae43a26eee4
facu.r <bruseghini.f@gmail.com>
2018-06-23 20:03:31 -0300

    add cellArgs parameter to importTemplate

--- components/definition/cellRules/ImportCellParams.js
+++ components/definition/cellRules/ImportCellParams.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import FontIcon from 'material-ui/FontIcon';
 import Checkbox from 'material-ui/Checkbox';
 import TextField from 'material-ui/TextField';
 import Dialog from 'material-ui/Dialog/Dialog';
+import IconButton from 'material-ui/IconButton';
 import FlatButton from 'material-ui/FlatButton/FlatButton';
 import { Card, CardTitle, CardText } from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton/RaisedButton';
 import Utils from '../../../Utils';
 import NetPyNEField from '../../general/NetPyNEField';
 import FileBrowser from '../../general/FileBrowser';
-
-var PythonControlledCapability = require('../../../../../js/communication/geppettoJupyter/PythonControlledCapability');
 
 export default class ImportCellParams extends React.Component {
 
@@ -21,10 +21,16 @@ export default class ImportCellParams extends React.Component {
       modFolder: '',
       compileMod: false,
       importSynMechs: false,
-      explorerDialogOpen: false
+      explorerDialogOpen: false,
+      
+      cellArgs: {},
+      newItemValue: ""
     };
     this.updateCheck = this.updateCheck.bind(this);
     this.performAction = this.performAction.bind(this);
+    
+    this.addChild = this.addChild.bind(this);
+    this.handleNewItemChange = this.handleNewItemChange.bind(this);
   };
 
   updateCheck(name) {
@@ -46,7 +52,8 @@ export default class ImportCellParams extends React.Component {
           conds : response,
           label : this.props.name,
           fileName : this.state.fileName,
-          cellName : this.state.cellName
+          cellName : this.state.cellName,
+          cellArgs: this.state.cellArgs
         };
 
         // Import template
@@ -83,6 +90,41 @@ export default class ImportCellParams extends React.Component {
 
     this.setState(newState);
   };
+  
+  isValid(value) {
+    return (value.match(/:/g)||[]).length==1 && !value.startsWith(":") && !value.endsWith(":");    
+  }
+
+  getErrorMessage() {
+    return 'Key:Value pairs must be separated by a colon --> : ';    
+  }
+
+  handleNewItemChange(event) {
+    this.setState({ newItemValue: event.target.value, newItemErrorText: '' })
+  }
+
+  addChild() {
+    if (this.isValid(this.state.newItemValue)) {
+      var cellArgs = this.state.cellArgs;
+      var newValue = this.state.newItemValue.split(":").map(entry => {
+        return entry
+      });
+      if (!isNaN(newValue[1])) { 
+        newValue[1] = parseFloat(newValue[1])
+      }
+      cellArgs[newValue[0]] = newValue[1];
+      this.setState({ cellArgs: cellArgs, newItemValue: "" });
+    } 
+    else {
+    this.setState({ newItemErrorText: this.getErrorMessage() })
+    }
+  }
+
+  removeChild(key) {
+    var cellArgs = this.state.cellArgs;
+    delete cellArgs[key];
+    this.setState({ cellArgs: cellArgs, newItemValue: "" });
+  }
 
   render() {
     const actions = [
@@ -97,6 +139,27 @@ export default class ImportCellParams extends React.Component {
         onTouchTap={this.performAction}
       />
     ];
+    
+    const cellArgs = Object.keys(this.state.cellArgs).map((key) => {
+      return <div key={key} style={{ marginRight: 30, float: 'left' }}>
+        <TextField
+          value={key + " : " + this.state.cellArgs[key]}
+          id={key}
+          // onChange={this.handleChildChange}
+          style={{ width: 100}}
+          inputStyle={{color:"rgb(2, 188, 212)"}}
+          disabled
+        />
+        <IconButton
+          iconStyle={{ width: 7, height: 7 }}
+          className={"listButtonSmall"}
+          onClick={() => this.removeChild(key)}
+          tooltip="Remove item from the list"
+        >
+          <FontIcon className={"fa fa-minus-circle listIcon"} />
+        </IconButton>
+      </div>
+    });
 
     return (
       <Dialog
@@ -128,6 +191,26 @@ export default class ImportCellParams extends React.Component {
                 onClick={() => this.showExplorerDialog('modFolder')} readOnly
               />
             </NetPyNEField>
+            
+            <TextField
+              floatingLabelText={"Add new key:value pair"}
+              onChange={this.handleNewItemChange}
+              value={this.state.newItemValue}
+              style={{ width: '90%' }}
+              errorText={this.state.newItemErrorText}
+            />
+            {!this.state.newItemErrorText &&
+              <IconButton
+                iconStyle={{ width: 10, height: 10 }}
+                className={"listButtonLarge"}
+                onClick={this.addChild}
+                tooltip="Add item to the dictionary"
+              >
+                <FontIcon className={"fa fa-plus-circle listIcon"} />
+              </IconButton>
+            }
+
+            {cellArgs.length > 0 && <div style={{marginTop:'15px', marginLeft: '50px', padding: '0px 5px', float: 'left' }}>{cellArgs}</div>}
 
             <div style={{ width: '100%', float: 'left', marginTop: '15px' }}>
               <div style={{ float: 'left', width: '50%' }}>
@@ -150,6 +233,8 @@ export default class ImportCellParams extends React.Component {
                 </NetPyNEField>
               </div>
             </div>
+            
+            
 
             <FileBrowser open={this.state.explorerDialogOpen} onRequestClose={(selection) => this.closeExplorerDialog(selection)} />
           </CardText>
