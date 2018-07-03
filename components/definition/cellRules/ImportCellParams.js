6fc090024848522ec19db4d4f5c926b200042b51
Adrian Quintana <adrianquintana@gmail.com>
2018-07-03 13:03:03 +0100

    refactor to use list component

--- components/definition/cellRules/ImportCellParams.js
+++ components/definition/cellRules/ImportCellParams.js
@@ -10,6 +10,8 @@ import RaisedButton from 'material-ui/RaisedButton/RaisedButton';
 import Utils from '../../../Utils';
 import NetPyNEField from '../../general/NetPyNEField';
 import FileBrowser from '../../general/FileBrowser';
+import ListComponent from '../../general/List';
+
 
 export default class ImportCellParams extends React.Component {
 
@@ -29,8 +31,6 @@ export default class ImportCellParams extends React.Component {
     this.updateCheck = this.updateCheck.bind(this);
     this.performAction = this.performAction.bind(this);
     
-    this.addChild = this.addChild.bind(this);
-    this.handleNewItemChange = this.handleNewItemChange.bind(this);
   };
 
   updateCheck(name) {
@@ -53,7 +53,7 @@ export default class ImportCellParams extends React.Component {
           label : this.props.name,
           fileName : this.state.fileName,
           cellName : this.state.cellName,
-          cellArgs: this.state.cellArgs
+          cellArgs: this.refs.cellArgs.state.children
         };
 
         // Import template
@@ -88,41 +88,6 @@ export default class ImportCellParams extends React.Component {
 
     this.setState(newState);
   };
-  
-  isValid(value) {
-    return (value.match(/:/g)||[]).length==1 && !value.startsWith(":") && !value.endsWith(":");    
-  }
-
-  getErrorMessage() {
-    return 'Key:Value pairs must be separated by a colon --> : ';    
-  }
-
-  handleNewItemChange(event) {
-    this.setState({ newItemValue: event.target.value, newItemErrorText: '' })
-  }
-
-  addChild() {
-    if (this.isValid(this.state.newItemValue)) {
-      var cellArgs = this.state.cellArgs;
-      var newValue = this.state.newItemValue.split(":").map(entry => {
-        return entry
-      });
-      if (!isNaN(newValue[1])) { 
-        newValue[1] = parseFloat(newValue[1])
-      }
-      cellArgs[newValue[0]] = newValue[1];
-      this.setState({ cellArgs: cellArgs, newItemValue: "" });
-    } 
-    else {
-    this.setState({ newItemErrorText: this.getErrorMessage() })
-    }
-  }
-
-  removeChild(key) {
-    var cellArgs = this.state.cellArgs;
-    delete cellArgs[key];
-    this.setState({ cellArgs: cellArgs, newItemValue: "" });
-  }
 
   render() {
     const actions = [
@@ -137,27 +102,6 @@ export default class ImportCellParams extends React.Component {
         onTouchTap={this.performAction}
       />
     ];
-    
-    const cellArgs = Object.keys(this.state.cellArgs).map((key) => {
-      return <div key={key} style={{ marginRight: 30, float: 'left' }}>
-        <TextField
-          value={key + " : " + this.state.cellArgs[key]}
-          id={key}
-          // onChange={this.handleChildChange}
-          style={{ width: 100}}
-          inputStyle={{color:"rgb(2, 188, 212)"}}
-          disabled
-        />
-        <IconButton
-          iconStyle={{ width: 7, height: 7 }}
-          className={"listButtonSmall"}
-          onClick={() => this.removeChild(key)}
-          tooltip="Remove item from the list"
-        >
-          <FontIcon className={"fa fa-minus-circle listIcon"} />
-        </IconButton>
-      </div>
-    });
 
     return (
       <Dialog
@@ -190,26 +134,10 @@ export default class ImportCellParams extends React.Component {
               />
             </NetPyNEField>
             
-            <TextField
-              floatingLabelText={"Cell Template Parameters (add new key:value pair)"}
-              onChange={this.handleNewItemChange}
-              value={this.state.newItemValue}
-              style={{ width: '90%' }}
-              errorText={this.state.newItemErrorText}
-            />
-            {!this.state.newItemErrorText &&
-              <IconButton
-                iconStyle={{ width: 10, height: 10 }}
-                className={"listButtonLarge"}
-                onClick={this.addChild}
-                tooltip="Add item to the dictionary"
-              >
-                <FontIcon className={"fa fa-plus-circle listIcon"} />
-              </IconButton>
-            }
-
-            {cellArgs.length > 0 && <div style={{marginTop:'15px', marginLeft: '50px', padding: '0px 5px', float: 'left' }}>{cellArgs}</div>}
-
+            <div className="listStyle netpyneField">
+              <ListComponent realType="dict" floatingLabelText="Cell Template Parameters (key:value pair)" ref="cellArgs"/>
+            </div>
+            
             <div style={{ width: '100%', float: 'left', marginTop: '15px' }}>
               <div style={{ float: 'left', width: '50%' }}>
                 <NetPyNEField id="netParams.importCellParams.importSynMechs" className="netpyneCheckbox netpyneFieldNoWidth" noStyle>
@@ -231,7 +159,6 @@ export default class ImportCellParams extends React.Component {
                 </NetPyNEField>
               </div>
             </div>
-            
             
 
             <FileBrowser open={this.state.explorerDialogOpen} exploreOnlyDirs={this.state.exploreOnlyDirs} onRequestClose={(selection) => this.closeExplorerDialog(selection)} />
