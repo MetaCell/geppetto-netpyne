f18d3083e562bbe4710a7fe4c20dc0570a678fd8
Adrian Quintana <adrianquintana@gmail.com>
2018-09-27 22:26:49 +0100

    fix highlight

--- components/settings/NetPyNETabs.js
+++ components/settings/NetPyNETabs.js
@@ -12,24 +12,33 @@ export default class NetPyNETabs2 extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            tabLabel: 'Create network',
+            simulateTabLabel: 'Create network',
+            label: 'define',
             transitionOptionsHovered: false
         }
 
         this.handleTransitionOptionsChange = this.handleTransitionOptionsChange.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        
+    }
+
+    handleChange(value){
+        this.setState({label: value});
+        this.props.handleChange(value)
     }
 
     handleTransitionOptionsChange(e,v) {
-        this.setState({tabLabel: v});
+        this.setState({simulateTabLabel: v});
         this.props.handleTransitionOptionsChange(e,v)
     }
+
     render() {
 
         return <div style={{width: '100%', alignItems: 'center', display: 'flex'}}>
-            <FlatButton onClick={()=>this.props.handleChange('define')} style={{flex: 1, borderRadius: 10}} backgroundColor={cyan500} hoverColor={cyan400} labelStyle={{color: this.state.value=='define'?'#ffffff':grey300}} label="Define your Network" />
-            <FlatButton onClick={()=>this.props.handleChange('simulate')} style={{flex: 1, borderRadius: 10}} backgroundColor={this.state.transitionOptionsHovered?cyan400:cyan500} hoverColor={cyan400} labelStyle={{color: this.state.value=='simulate'?'#ffffff':grey300}} label={this.state.tabLabel} />
+            <FlatButton onClick={()=>this.handleChange('define')} style={{flex: 1, borderRadius: 10}} backgroundColor={cyan500} hoverColor={cyan400} labelStyle={{color: this.state.label=='define'?'#ffffff':grey300}} label="Define your Network" />
+            <FlatButton onClick={()=>this.handleChange('simulate')} style={{flex: 1, borderRadius: 10}} backgroundColor={this.state.transitionOptionsHovered?cyan400:cyan500} hoverColor={cyan400} labelStyle={{color: this.state.label=='simulate'?'#ffffff':grey300}} label={this.state.simulateTabLabel} />
             <IconMenu
-                value={this.state.tabLabel} 
+                value={this.state.simulateTabLabel} 
                 iconStyle={{color: '#ffffff'}} 
                 style={{position: 'absolute', top:'6px', right: '28px'}} 
                 iconButtonElement={<IconButton onMouseEnter={()=>this.setState({transitionOptionsHovered: true})} onMouseLeave={()=>this.setState({transitionOptionsHovered: false})}><NavigationExpandMoreIcon /></IconButton>} 
