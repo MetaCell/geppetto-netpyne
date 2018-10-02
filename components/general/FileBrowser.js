e59b5110b72a8131090168cbeca4904ac6dfe342
Adrian Quintana <adrianquintana@gmail.com>
2018-10-02 09:17:44 +0100

    set property values by ddefault

--- components/general/FileBrowser.js
+++ components/general/FileBrowser.js
@@ -21,7 +21,7 @@ export default class FileBrowser extends React.Component {
         }
 
         Utils
-            .sendPythonMessage('netpyne_geppetto.getDirList', [path, this.props.exploreOnlyDirs!=undefined?this.props.exploreOnlyDirs:false, this.props.onlyFiles!=undefined?this.props.onlyFiles:false])
+            .sendPythonMessage('netpyne_geppetto.getDirList', [path, this.props.exploreOnlyDirs, this.props.onlyFiles])
             .then((dirList) => {
                 if (treeData != [] && treeData.length > 0) {
                     rowInfo.node.children = dirList;
@@ -102,6 +102,10 @@ export default class FileBrowser extends React.Component {
     }
 };
 
+FileBrowser.defaultProps = {
+    exploreOnlyDirs: false,
+    onlyFiles: false
+};
 
 
 
