306c9028537e4ab9944f9073dfb5eb1b0ca3fb16
Adrian Quintana <adrianquintana@gmail.com>
2018-11-12 09:05:13 +0000

    remove commented code, wrong indentation, extract styles

--- components/definition/cellRules/ImportCellParams.js
+++ components/definition/cellRules/ImportCellParams.js
@@ -10,13 +10,40 @@ import NetPyNEField from '../../general/NetPyNEField';
 import FileBrowser from '../../general/FileBrowser';
 import ListComponent from '../../general/List';
 
+const styles = {
+  card: {
+    main: {
+      padding: 10, float: 'left', width: '100%'
+    },
+    title: {
+      paddingBottom: 0
+    },
+    cancel: {
+      marginRight: 16
+    }
+  },
+  mods: {
+    container: {
+      width: '100%', float: 'left', marginTop: '15px'
+    },
+    leftSubContainer: {
+      float: 'left', width: '50%'
+    },
+    rightSubContainer: {
+      float: 'right', width: '50%'
+    },
+    checkbox: {
+      width: '90%'
+    }
+  }
+}
 
 export default class ImportCellParams extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-			name: 'Cell_Rule_Name',
+      name: 'Cell_Rule_Name',
       open: true,
       fileName: '',
       cellName: '',
@@ -35,42 +62,35 @@ export default class ImportCellParams extends React.Component {
 
   updateCheck(name) {
     this.setState(({[name]: pv}) => ({ [name]: !pv}));
-	};
-
-	
-	// componentDidUpdate(nextProps, nextState) {
-	// 	if (nextProps.open != this.props.open) {
-	// 		this.setState({open: true})
-	// 	}
-	// }
+  };
 
   processError(response) {
     var parsedResponse = Utils.getErrorResponse(response);
     if (parsedResponse) {
-			GEPPETTO.trigger(GEPPETTO.Events.Hide_spinner);
-			this.setState({ open: true, errorMessage: parsedResponse['message'], errorDetails: parsedResponse['details'] })
-			return true;
+      GEPPETTO.trigger(GEPPETTO.Events.Hide_spinner);
+      this.setState({ open: true, errorMessage: parsedResponse['message'], errorDetails: parsedResponse['details'] })
+      return true;
     }
     return false;
   };
 
   performAction =  async () => {
-		const cellArgs = this.refs.cellArgs.state.children;
-		const { name, fileName, cellName, modFolder, compileMod } = this.state;
+    const cellArgs = this.refs.cellArgs.state.children;
+    const { name, fileName, cellName, modFolder, compileMod } = this.state;
 
-		this.closeImportCellParams();
-		GEPPETTO.trigger(GEPPETTO.Events.Show_spinner, GEPPETTO.Resources.IMPORTING_MODEL);
+    this.closeImportCellParams();
+    GEPPETTO.trigger(GEPPETTO.Events.Show_spinner, GEPPETTO.Resources.IMPORTING_MODEL);
 
     const conds = await Utils.evalPythonMessage(`netpyne_geppetto.netParams.cellParams['${name}']['conds'] if '${name}' in netpyne_geppetto.netParams.cellParams else {}`)
 
     const data = { conds, cellArgs, fileName, cellName, label: name };
 
-		const response = await Utils.evalPythonMessage('netpyne_geppetto.importCellTemplate', [data, modFolder, compileMod])
+    const response = await Utils.evalPythonMessage('netpyne_geppetto.importCellTemplate', [data, modFolder, compileMod])
 
-		if (!this.processError(response)) {
-			GEPPETTO.CommandController.log("The cell params were imported");
-			GEPPETTO.trigger(GEPPETTO.Events.Hide_spinner);
-		}
+    if (!this.processError(response)) {
+      GEPPETTO.CommandController.log("The cell params were imported");
+      GEPPETTO.trigger(GEPPETTO.Events.Hide_spinner);
+    }
   };
 
   closeImportCellParams() {
@@ -105,98 +125,98 @@ export default class ImportCellParams extends React.Component {
   };
 
   render() {
-		const { open, name, fileName, cellName, modFolder, importSynMechs, compileMod, errorMessage, errorDetails, explorerDialogOpen, exploreOnlyDirs } = this.state;
+    const { open, name, fileName, cellName, modFolder, importSynMechs, compileMod, errorMessage, errorDetails, explorerDialogOpen, exploreOnlyDirs } = this.state;
     if (open) {
       var cancelAction = <FlatButton
         label={'CANCEL'}
-				style={{ marginRight: 16 }}
-				onClick={this.cancelImportCellParams}
+        onClick={this.cancelImportCellParams}
+        style={styles.card.cancel}
       />
       if (errorMessage == undefined) {
         var actions = [
           cancelAction,
           <RaisedButton
-						primary
-						label={"IMPORT"}
-						id="acceptImportCellTemplate"
+            primary
+            label={"IMPORT"}
+            id="acceptImportCellTemplate"
             onClick={this.performAction}
           />
         ];
         var children = (
-					<Card style={styles.card.main}>
-						<CardTitle 
-							style={styles.card.title}
-							title="Import Cell Template"
-							subtitle="Python or Hoc files"
-						/>
-						<CardText>
-							<TextField
-									value={name}
-									id="importCellTemplateName"
-									floatingLabelText="Cell rule label"
-									onChange={event => this.setState({ name: event.target.value })}
-							/>
-							<NetPyNEField id="netParams.importCellParams.fileName" className="netpyneFieldNoWidth">
-								<TextField
-									readOnly
-									value={fileName}
-									id="importCellTemplateFile"
-									onClick={() => this.showExplorerDialog('fileName', false)}
-								/>
-							</NetPyNEField>
+          <Card style={styles.card.main}>
+            <CardTitle 
+              style={styles.card.title}
+              title="Import Cell Template"
+              subtitle="Python or Hoc files"
+            />
+            <CardText>
+              <TextField
+                  value={name}
+                  id="importCellTemplateName"
+                  floatingLabelText="Cell rule label"
+                  onChange={event => this.setState({ name: event.target.value })}
+              />
+              <NetPyNEField id="netParams.importCellParams.fileName" className="netpyneFieldNoWidth">
+                <TextField
+                  readOnly
+                  value={fileName}
+                  id="importCellTemplateFile"
+                  onClick={() => this.showExplorerDialog('fileName', false)}
+                />
+              </NetPyNEField>
 
-							<NetPyNEField id="netParams.importCellParams.cellName" className="netpyneRightField">
-								<TextField
-									value={cellName}
-									id="importCellTemplateCellName"
-									onChange={event => this.setState({ cellName: event.target.value })}
-								/>
-							</NetPyNEField>
+              <NetPyNEField id="netParams.importCellParams.cellName" className="netpyneRightField">
+                <TextField
+                  value={cellName}
+                  id="importCellTemplateCellName"
+                  onChange={event => this.setState({ cellName: event.target.value })}
+                />
+              </NetPyNEField>
 
-							<NetPyNEField id="netParams.importCellParams.modFolder" className="netpyneRightField" >
-								<TextField
-									readOnly
-									value={modFolder}
-									id="importCellTemplateModFile"
-									onClick={() => this.showExplorerDialog('modFolder', true)} 
-								/>
-							</NetPyNEField>
+              <NetPyNEField id="netParams.importCellParams.modFolder" className="netpyneRightField" >
+                <TextField
+                  readOnly
+                  value={modFolder}
+                  id="importCellTemplateModFile"
+                  onClick={() => this.showExplorerDialog('modFolder', true)} 
+                />
+              </NetPyNEField>
 
-							<div className="listStyle netpyneField">
-								<ListComponent realType="dict" floatingLabelText="Cell Template Parameters (key:value pair)" ref="cellArgs" />
-							</div>
+              <div className="listStyle netpyneField">
+                <ListComponent realType="dict" floatingLabelText="Cell Template Parameters (key:value pair)" ref="cellArgs" />
+              </div>
 
-							<div style={styles.mods.container}>
-								<div style={styles.mods.leftSubContainer}>
-									<NetPyNEField id="netParams.importCellParams.importSynMechs" className="netpyneCheckbox netpyneFieldNoWidth" noStyle>
-										<Checkbox
-											checked={importSynMechs}
-											style={styles.mods.checkbox}
-											onCheck={() => this.updateCheck('importSynMechs')}
-										/>
-									</NetPyNEField>
-								</div>
+              <div style={styles.mods.container}>
+                <div style={styles.mods.leftSubContainer}>
+                  <NetPyNEField id="netParams.importCellParams.importSynMechs" className="netpyneCheckbox netpyneFieldNoWidth" noStyle>
+                    <Checkbox
+                      checked={importSynMechs}
+                      style={styles.mods.checkbox}
+                      onCheck={() => this.updateCheck('importSynMechs')}
+                    />
+                  </NetPyNEField>
+                </div>
 
-								<div style={styles.mods.rightSubContainer}>
-									<NetPyNEField id="netParams.importCellParams.compileMod" className="netpyneCheckbox netpyneFieldNoWidth" noStyle>
-										<Checkbox
-											checked={compileMod}
-											style={styles.mods.checkbox}
-											id="importCellTemplateCompileMods"
-											onCheck={() => this.updateCheck('compileMod')}
-										/>
-									</NetPyNEField>
-								</div>
-							</div>
+                <div style={styles.mods.rightSubContainer}>
+                  <NetPyNEField id="netParams.importCellParams.compileMod" className="netpyneCheckbox netpyneFieldNoWidth" noStyle>
+                    <Checkbox
+                      checked={compileMod}
+                      style={styles.mods.checkbox}
+                      id="importCellTemplateCompileMods"
+                      onCheck={() => this.updateCheck('compileMod')}
+                    />
+                  </NetPyNEField>
+                </div>
+              </div>
 
-							<FileBrowser 
-								open={explorerDialogOpen} 
-								exploreOnlyDirs={exploreOnlyDirs} 
-								onRequestClose={selection => this.closeExplorerDialog(selection)}
-							/>
-						</CardText>
-        	</Card>
-				)
+              <FileBrowser 
+                open={explorerDialogOpen} 
+                exploreOnlyDirs={exploreOnlyDirs} 
+                onRequestClose={selection => this.closeExplorerDialog(selection)}
+              />
+            </CardText>
+          </Card>
+        )
       }
       else {
         var actions = [
@@ -225,28 +245,3 @@ export default class ImportCellParams extends React.Component {
     return null;
   };
 };
-
-const styles = {
-	card: {
-		main: {
-			padding: 10, float: 'left', width: '100%'
-		},
-		title: {
-			paddingBottom: 0
-		}
-	},
-	mods: {
-		container: {
-			width: '100%', float: 'left', marginTop: '15px'
-		},
-		leftSubContainer: {
-			float: 'left', width: '50%'
-		},
-		rightSubContainer: {
-			float: 'right', width: '50%'
-		},
-		checkbox: {
-			width: '90%'
-		}
-	}
-}
