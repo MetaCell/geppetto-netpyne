4043081e825874530aa38640cc5a869cd61ac39e
rodriguez-facundo <bruseghini.f@gmail.com>
2018-11-12 18:48:27 -0300

    fix tab-indent issue on github (5 try)

--- components/settings/NetPyNEToolBar.js
+++ components/settings/NetPyNEToolBar.js
@@ -18,95 +18,95 @@ const ImportIcon = props => <SvgIcon {...props}><svg viewBox='0 0 750 750'><path
 const CellTemplateIcon = props => <SvgIcon {...props}><svg viewBox="0 0 448 512"><path d="M448 75.2v361.7c0 24.3-19 43.2-43.2 43.2H43.2C19.3 480 0 461.4 0 436.8V75.2C0 51.1 18.8 32 43.2 32h361.7c24 0 43.1 18.8 43.1 43.2zm-37.3 361.6V75.2c0-3-2.6-5.8-5.8-5.8h-9.3L285.3 144 224 94.1 162.8 144 52.5 69.3h-9.3c-3.2 0-5.8 2.8-5.8 5.8v361.7c0 3 2.6 5.8 5.8 5.8h361.7c3.2.1 5.8-2.7 5.8-5.8zM150.2 186v37H76.7v-37h73.5zm0 74.4v37.3H76.7v-37.3h73.5zm11.1-147.3l54-43.7H96.8l64.5 43.7zm210 72.9v37h-196v-37h196zm0 74.4v37.3h-196v-37.3h196zm-84.6-147.3l64.5-43.7H232.8l53.9 43.7zM371.3 335v37.3h-99.4V335h99.4z"></path></svg></SvgIcon>;
 
 export default class NetPyNEToolBar extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			openDialogBox: false,
-			open: false,
-			action: null
-		}		
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      openDialogBox: false,
+      open: false,
+      action: null
+    }		
+  }
 
-	handleMenuItemClick = (action) => {
-		this.setState({action:action, openDialogBox:true, open: false})   
-	}
+  handleMenuItemClick = (action) => {
+    this.setState({action:action, openDialogBox:true, open: false})   
+  }
 
-	render() {
+  render() {
 
-		if (this.state.openDialogBox){
-			switch(this.state.action){
-				case 'Load':
-					var content = <LoadFile
-							open={this.state.openDialogBox}
-							onRequestClose={() => this.setState({ openDialogBox: false })}
-							changeTab={this.props.changeTab}
-					/>
-					break;
-				case 'Save':
-					var content = <SaveFile
-							open={this.state.openDialogBox}    
-							onRequestClose={() => this.setState({ openDialogBox: false })}
-							changeTab={this.props.changeTab}
-							/>
-					break;
-				case 'ImportHLS':
-					var content = <ImportExportHLS 
-							open={this.state.openDialogBox}
-							onRequestClose={() => this.setState({ openDialogBox: false })}
-							changeTab={this.props.changeTab}
-							mode ={"IMPORT"}/>
-					break;
-				case 'ExportHLS':
-					var content = <ImportExportHLS 
-							open={this.state.openDialogBox}
-							onRequestClose={() => this.setState({ openDialogBox: false })}
-							changeTab={this.props.changeTab}
-							mode ={"EXPORT"}
-					/>
-					break;
-				case 'ImportCellTemplate':
-					var content = <ImportCellParams
-							open={this.state.openDialogBox}
-							onRequestClose={() => this.setState({ openDialogBox: false })}
-					/>
-					break;
-				case 'Remove':
-					var content = <DeleteWork
-							open={this.state.openDialogBox}
-							onRequestClose={() => this.setState({ openDialogBox: false })}
-							changeTab={this.props.changeTab}
-					/>
-					break;
-			}
-		}
-				
-		return <div>
-			<IconButton
-				id="appBar"
-				tooltip={'File options'}
-				style={{ width: 40, height: 40, borderRadius: 25, overflow: 'hidden' }}
-				iconStyle={{ color: '#ffffff', marginTop: -4, marginLeft: -4 }}
-				hoveredStyle={{ backgroundColor: '#26C6DA' }}
-				onClick={() => this.setState({ open: !this.state.open })}
-			>
-				<NavigationMenu />
-			</IconButton>
-			<Drawer
-				docked={false}
-				width={265}
-				open={this.state.open}
-				onRequestChange={(open) => this.setState({ open })}
-			>
-				<img style={{ marginLeft: 25, marginTop: 5, marginBottom: 8, width: 205 }} src={NetPyNElogo} />
-				<Divider />
-				<MenuItem id="appBarOpen" primaryText="Open..." onClick={() => this.handleMenuItemClick('Load')} leftIcon={<FontIcon className='fa fa-folder-open-o' />} />
-				<MenuItem id="appBarSave" primaryText="Save..." onClick={() => this.handleMenuItemClick('Save')} leftIcon={<FontIcon className='fa fa-download' />} />
-				<MenuItem id="appBarImportHLS" primaryText="Import HLS" onClick={() => this.handleMenuItemClick('ImportHLS')} leftIcon={<ImportIcon />} />
-				<MenuItem id="appBarExportHLS" primaryText="Export HLS" onClick={() => this.handleMenuItemClick('ExportHLS')} leftIcon={<ExportIcon />} />
-				<MenuItem id="appBarImportCellTemplate" primaryText="Import Cell" onClick={() => this.handleMenuItemClick('ImportCellTemplate')} leftIcon={<CellTemplateIcon />} />
-				<MenuItem id="appBarDelete" primaryText="Remove current project" onClick={() => this.handleMenuItemClick('Remove')} leftIcon={<FontIcon className='fa fa-trash-o' />} />
-			</Drawer>
-			{content}
-		</div>
-		}
+    if (this.state.openDialogBox){
+      switch(this.state.action){
+        case 'Load':
+          var content = <LoadFile
+              open={this.state.openDialogBox}
+              onRequestClose={() => this.setState({ openDialogBox: false })}
+              changeTab={this.props.changeTab}
+          />
+          break;
+        case 'Save':
+          var content = <SaveFile
+              open={this.state.openDialogBox}    
+              onRequestClose={() => this.setState({ openDialogBox: false })}
+              changeTab={this.props.changeTab}
+              />
+          break;
+        case 'ImportHLS':
+          var content = <ImportExportHLS 
+              open={this.state.openDialogBox}
+              onRequestClose={() => this.setState({ openDialogBox: false })}
+              changeTab={this.props.changeTab}
+              mode ={"IMPORT"}/>
+          break;
+        case 'ExportHLS':
+          var content = <ImportExportHLS 
+              open={this.state.openDialogBox}
+              onRequestClose={() => this.setState({ openDialogBox: false })}
+              changeTab={this.props.changeTab}
+              mode ={"EXPORT"}
+          />
+          break;
+        case 'ImportCellTemplate':
+          var content = <ImportCellParams
+              open={this.state.openDialogBox}
+              onRequestClose={() => this.setState({ openDialogBox: false })}
+          />
+          break;
+        case 'Remove':
+          var content = <DeleteWork
+              open={this.state.openDialogBox}
+              onRequestClose={() => this.setState({ openDialogBox: false })}
+              changeTab={this.props.changeTab}
+          />
+          break;
+      }
+    }
+        
+    return <div>
+      <IconButton
+        id="appBar"
+        tooltip={'File options'}
+        style={{ width: 40, height: 40, borderRadius: 25, overflow: 'hidden' }}
+        iconStyle={{ color: '#ffffff', marginTop: -4, marginLeft: -4 }}
+        hoveredStyle={{ backgroundColor: '#26C6DA' }}
+        onClick={() => this.setState({ open: !this.state.open })}
+      >
+        <NavigationMenu />
+      </IconButton>
+      <Drawer
+        docked={false}
+        width={265}
+        open={this.state.open}
+        onRequestChange={(open) => this.setState({ open })}
+      >
+        <img style={{ marginLeft: 25, marginTop: 5, marginBottom: 8, width: 205 }} src={NetPyNElogo} />
+        <Divider />
+        <MenuItem id="appBarOpen" primaryText="Open..." onClick={() => this.handleMenuItemClick('Load')} leftIcon={<FontIcon className='fa fa-folder-open-o' />} />
+        <MenuItem id="appBarSave" primaryText="Save..." onClick={() => this.handleMenuItemClick('Save')} leftIcon={<FontIcon className='fa fa-download' />} />
+        <MenuItem id="appBarImportHLS" primaryText="Import HLS" onClick={() => this.handleMenuItemClick('ImportHLS')} leftIcon={<ImportIcon />} />
+        <MenuItem id="appBarExportHLS" primaryText="Export HLS" onClick={() => this.handleMenuItemClick('ExportHLS')} leftIcon={<ExportIcon />} />
+        <MenuItem id="appBarImportCellTemplate" primaryText="Import Cell" onClick={() => this.handleMenuItemClick('ImportCellTemplate')} leftIcon={<CellTemplateIcon />} />
+        <MenuItem id="appBarDelete" primaryText="Remove current project" onClick={() => this.handleMenuItemClick('Remove')} leftIcon={<FontIcon className='fa fa-trash-o' />} />
+      </Drawer>
+      {content}
+    </div>
+    }
 }
